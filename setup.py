fd05f586964c2dcbcb9a8c143fb35784169440e2
AkashSri13 <163294332+AkashSri13@users.noreply.github.com>
2024-03-28 01:57:55 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''This function will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+name = 'End to End Machine Learning Project - Student Performance Prediction',
+version = '0.0.1',
+author = 'Akash Srivastava',
+author_email = 'aksr.sri@gmail.com',
+packages=find_packages(),
+intall_requires=get_requirements('requirements.txt')
+)
